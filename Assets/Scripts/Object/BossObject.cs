7f4174458b80cfd466af831c092d687cde0b1dcd
我是原神职业哥 <139077683+a910126@users.noreply.github.com>
2024-07-26 01:10:40 +0800

    Boss的攻击的概率

--- Assets/Scripts/Object/BossObject.cs
+++ Assets/Scripts/Object/BossObject.cs
@@ -19,6 +19,11 @@ public class BossObject : ObjectBase
     /// </summary>
     private bool IsHurt;
 
+    /// <summary>
+    /// 攻击方式的概率
+    /// </summary>
+    private int[] ProbabilityArray;
+
     protected override void Awake()
     {
         base.Awake();
@@ -39,32 +44,74 @@ public class BossObject : ObjectBase
         if (Ai != null)
             Ai.UpdateState();  //执行Ai
     }
-    public override void StandBy()
-    {
-        Animator.SetBool("IsWalk", false);  //切动画
-    }
-
-    public void Move()
-    {
-        Animator.SetBool("IsWalk", true);
-    }
-    public override void Atk()  //攻击
-    {
-        Animator.SetBool("IsAtk1", true);
-    }
-
-    private void AfterAtk()  //攻击结束后
-    {
-        Animator.SetBool("IsAtk1", false);
-        StandBy();
-    }
-   
 
     public override void InitShuXing()  //初始化属性
     {
         ShuXing = new ShuXing_Monster(1);
         MonsterShuXing = ShuXing as ShuXing_Monster;
+
+        ProbabilityArray = new int[3] { 50, 25, 25 };
     }
+    public override void StandBy()  //待机
+    {
+        Animator.SetBool("IsWalk", false);  //切动画
+    }
+
+    public void Move()  //移动
+    {
+        Animator.SetBool("IsWalk", true);
+    }
+
+    private int AtkProbability(int[] probabilityArray,int total)  //攻击概率
+    {
+        int r = Random.Range(0, total + 1);
+        int t = 0;
+        for(int i=0;i<probabilityArray.Length;i++)
+        {
+            t += probabilityArray[i];
+            if(r<t)
+            return i;
+        }
+        return 0;
+    }
+    public override void Atk()  //攻击
+    {
+        
+
+        switch (AtkProbability(ProbabilityArray, 100))
+        {
+            case 0:
+                Animator.SetBool("IsAtk1", true);
+                print("Atk1");
+                break;
+            case 1:
+                Animator.SetBool("IsAtk2", true);
+                print("Atk2");
+                break;
+            case 2:
+                Animator.SetBool("IsAtk3", true);
+                print("Atk3");
+                break;
+        }
+    }
+
+    private void AfterAtk1()  //攻击结束后
+    {
+        Animator.SetBool("IsAtk1", false);
+        StandBy();
+    }
+
+    private void AfterAtk2()  //攻击结束后
+    {
+        Animator.SetBool("IsAtk2", false);
+        StandBy();
+    }
+    private void AfterAtk3()  //攻击结束后
+    {
+        Animator.SetBool("IsAtk3", false);
+        StandBy();
+    }
+
 
     public override void Hurt(float value)   //受伤
     {
